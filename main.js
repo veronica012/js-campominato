a0176868739785234ccdab7ff8eb961f6295de3e
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-27 16:36:01 +0200

    funzione random

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
 // Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine"
 var array_random = [];
 while (array_random.length < 16) {
-    var numero_random =(Math.floor(Math.random()*101)+1);
+    var numero_random = getRandom(1, 100);
     if (array_random.includes(numero_random) == false) {
         array_random.push(numero_random);
     }
 }
 console.log(array_random);
+
 // In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
 // Se il numero è presente nella lista delle mine, la partita termina, altrimenti il gioco continua chiedendo all'utente un altro numero (continua a giocare).
 //partita termina quando il giocatore becca una mina, ossia inserisce un numero "vietato", oppure se raggiunge il numero massimo possibile di numeri consentiti, ossia ha inserito tutti i numeri possibili che non sono mine!
