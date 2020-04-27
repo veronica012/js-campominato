c98afad8f17be82094fa86b82d645fd0a7e6cc90
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-27 17:11:24 +0200

    condizioni bonus

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 // BONUS: all'inizio della partita, il software richiede anche un livello di difficoltà all'utente che cambia il range di numeri totali (le mine saranno sempre 16):
-// con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
-var level = prompt('')
-//var numero_random = getRandom(1, 100);
-// con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
-//var numero_random = getRandom(1, 80);
-// con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
-//var numero_random = getRandom(1, 100)
+
+var level = prompt('Scegli un livello di difficoltà: 0, 1 o 2');
+console.log(level);
+if (level == '0') {
+    var numero_random = getRandom(1, 100)
+} else if (level == '1') {
+    var numero_random = getRandom(1, 80)
+} else {
+    var numero_random = getRandom(1, 50)
+}
+
 // Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine"
 var array_random = [];
 while (array_random.length < 16) {
@@ -17,7 +21,7 @@ while (array_random.length < 16) {
         array_random.push(numero_random);
     }
 }
-//console.log(array_random);
+console.log(array_random);
 
 // In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
 // Se il numero è presente nella lista delle mine, la partita termina, altrimenti il gioco continua chiedendo all'utente un altro numero (continua a giocare).
@@ -33,5 +37,6 @@ while(numeri_consentiti.length < possibilita && array_random.includes(numero_ute
         console.log('Hai colpito la bomba con il numero ' + numero_utente)
     }
 }
+
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere
 console.log('Il tuo punteggio è ' + numeri_consentiti.length);
