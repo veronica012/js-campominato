28553c01363ae0d25c0e06a3ecdb00088d1039d7
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-27 16:56:44 +0200

    inizio bonus

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+// BONUS: all'inizio della partita, il software richiede anche un livello di difficoltà all'utente che cambia il range di numeri totali (le mine saranno sempre 16):
+// con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
+var level = prompt('')
+//var numero_random = getRandom(1, 100);
+// con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
+//var numero_random = getRandom(1, 80);
+// con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
+//var numero_random = getRandom(1, 100)
 // Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine"
 var array_random = [];
 while (array_random.length < 16) {
@@ -27,8 +35,3 @@ while(numeri_consentiti.length < possibilita && array_random.includes(numero_ute
 }
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere
 console.log('Il tuo punteggio è ' + numeri_consentiti.length);
-
-// BONUS: all'inizio della partita, il software richiede anche un livello di difficoltà all'utente che cambia il range di numeri totali (le mine saranno sempre 16):
-// con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
-// con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
-// con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
